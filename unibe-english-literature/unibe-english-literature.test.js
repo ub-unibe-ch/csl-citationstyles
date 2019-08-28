3870f7a593437b98776d12fa56785b7c1b3e0c88
denismaier <maier.de@gmail.com>
2019-08-28 22:34:42 +0200

    unibe-english-literature entfernen aus master

--- unibe-english-literature/unibe-english-literature.test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  csl: "./unibe-english-literature/unibe-english-literature.csl",
-  libraries: ["./unibe-english-literature/corpus.json"],
-  suites: ["./unibe-english-literature/unibe-english-literature.tests.yaml"]
-};
-
-const { jestCSL } = require('jest-csl');
-jestCSL(module.exports);
